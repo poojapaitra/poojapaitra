77c5e6fd1e1ee8420e4303426958c015a348a60f
poojapaitra <40381193+poojapaitra@users.noreply.github.com>
2018-06-19 18:52:27 +0530

    Create Year.java

---
+++ Year.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class Year
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        if(a%4==0)
+        {
+            System.out.print("yes");
+        }
+        else
+        {
+            System.out.print("no");
+        }
+        
+    }
+}
+
+
