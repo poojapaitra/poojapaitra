27864a5e28c20e3f0200fdc5130590bd5e591ea1
poojapaitra <40381193+poojapaitra@users.noreply.github.com>
2018-06-19 18:56:47 +0530

    Create Cou.java

---
+++ Cou.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class Cou
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        for(int i=0;i<a;i++)
+        {
+            System.out.println("hello");
+        }
+        
+    }
+}
+
+
